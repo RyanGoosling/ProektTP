20f84e5e21726f1d037a4f84efe22d744f2463dd
RyanGoosling <goosev0801@gmail.com>
2022-03-20 14:45:10 +0300

    many clients

--- mytcpserver.cpp
+++ mytcpserver.cpp
@@ -5,38 +5,79 @@
 MyTcpServer::~MyTcpServer()
 {
     mTcpServer->close();
-    //server_status=0;
 }
-MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
-    mTcpServer = new QTcpServer(this);
-    connect(mTcpServer, &QTcpServer::newConnection,
-            this, &MyTcpServer::slotNewConnection);
 
-    if(!mTcpServer->listen(QHostAddress::Any, 33333)){
+MyTcpServer::MyTcpServer(){//(QObject *parent) : QObject(parent){ //Убрано Вадим
+//    mTcpServer = new QTcpServer(this);
+//    connect(mTcpServer, &QTcpServer::newConnection,
+//            this, &MyTcpServer::slotNewConnection);
+
+    if(!this->listen(QHostAddress::Any, 12345))
+    {
         qDebug() << "server is not started";
-    } else {
-        //server_status=1;
+    }
+    else
+    {
         qDebug() << "server is started";
     }
 }
 
-void MyTcpServer::slotNewConnection(){
- //   if(server_status==1){
-        mTcpSocket = mTcpServer->nextPendingConnection();
-        mTcpSocket->write("Hello, World!!! I am echo server!\r\n");
-        connect(mTcpSocket, &QTcpSocket::readyRead,this,&MyTcpServer::slotServerRead);
-        connect(mTcpSocket,&QTcpSocket::disconnected,this,&MyTcpServer::slotClientDisconnected);
-   // }
+void MyTcpServer::incomingConnection(qintptr socketDescriptor) //Вадим
+{
+    socket = new QTcpSocket;
+    socket->setSocketDescriptor(socketDescriptor);
+    connect(socket, &QTcpSocket::readyRead, this, &MyTcpServer::slotServerRead);
+    connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
+
+    Sockets.push_back(socket);
+    qDebug() << "Client connection" << socketDescriptor;
 }
 
+//void MyTcpServer::slotReadyRead() //Вадим
+//{
+//    socket = (QTcpSocket*)sender();
+//    QDataStream in(socket);
+//    in.setVersion(QDataStream::Qt_5_9);
+//    if(in.status() == QDataStream::Ok)
+//    {
+//        qDebug() << "read...";
+//        QString str;
+//        in >> str;
+//        qDebug() << str;
+//    }
+//    else
+//    {
+//        qDebug() << "DataStream error";
+//    }
+//}
+
+//void MyTcpServer::SendToClient(QString str) //Вадим
+//{
+//    Data.clear();
+//    QDataStream out(&Data, QIODevice::WriteOnly);
+//    out.setVersion(QDataStream::Qt_5_9);
+//    out << str;
+//    socket->write(Data);
+//}
+
+//void MyTcpServer::slotNewConnection(){ //Убрано Вадим
+// //   if(server_status==1){
+//        socket = mTcpServer->nextPendingConnection();
+//        socket->write("Hello, World!!! I am echo server!\r\n");
+//        connect(socket, &QTcpSocket::readyRead,this,&MyTcpServer::slotServerRead);
+//        connect(socket,&QTcpSocket::disconnected,this,&MyTcpServer::slotClientDisconnected);
+//   // }
+//}
+
 void MyTcpServer::slotServerRead(){
-    while(mTcpSocket->bytesAvailable()>0)
+    socket = (QTcpSocket*)sender(); //Добавлено Вадим
+    while(socket->bytesAvailable()>0)
     {
-        QByteArray array =mTcpSocket->readAll();
-        mTcpSocket->write(array);
+        QByteArray array = socket->readAll();
+        socket->write(array);
     }
 }
 
-void MyTcpServer::slotClientDisconnected(){
-    mTcpSocket->close();
-}
+//void MyTcpServer::slotClientDisconnected(){
+//    socket->close();
+//}
--- mytcpserver.h
+++ mytcpserver.h
@@ -8,21 +8,29 @@
 #include <QByteArray>
 #include <QDebug>
 
-class MyTcpServer : public QObject
+class MyTcpServer : public QTcpServer //QObject//Убрано В
 {
     Q_OBJECT
 public:
-    explicit MyTcpServer(QObject *parent = nullptr);
+    /*explicit*/ MyTcpServer();//(QObject *parent = nullptr);//Убрано В
+    QTcpSocket * socket; //В
     ~MyTcpServer();
 public slots:
-    void slotNewConnection();
-    void slotClientDisconnected();
+
+    void incomingConnection(qintptr socketDescriptor); //В
+    //void slotReadyRead(); //Убрано В
+
+    //void slotNewConnection(); //Убрано В
+    //void slotClientDisconnected(qintptr socketDescriptor);//Убрано В
 
     void slotServerRead();
+
 private:
     QTcpServer * mTcpServer;
-    QTcpSocket * mTcpSocket;
-    //int server_status;
+    QVector <QTcpSocket*> Sockets; //В
+    QByteArray Data; //В
+
+    //void SendToClient(QString str); //Убрано В
 };
 #endif // MYTCPSERVER_H
 
